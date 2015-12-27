5bcd51b8b0c89492cb5ed6c5551825fee3b267d1
Olovan <micahsmith.92@gmail.com>
2015-12-27 13:59:25 -0500

    Game now sets the game pointer in Entity in createEntity()

--- Game.cpp
+++ Game.cpp
@@ -9,5 +9,6 @@ Entity* Game::createEntity(Vector2 iPosition)
 {
     auto entity = std::make_shared<Entity>(iPosition);
     entities.push_back(entity);
+    entity.get()->game = this;
     return entity.get();
 }
