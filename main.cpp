d090f2d23b76b7bab081b7394becb9cfa2563896
Olovan <micahsmith.92@gmail.com>
2015-12-27 14:01:17 -0500

    removed old debug code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-#include "Entity.h"
-#include "Component.h"
+
 #include "Vector2.h"
-#include "Physics.h"
+#include "Game.h"
+    #include "Entity.h"
+        #include "Component.h"
+            #include "Physics.h"
+            #include "Renderer.h"
 
 using namespace std;
 
@@ -20,24 +23,6 @@ public:
 int main()
 {
    //TEST CODE
-   Vector2 v1(5,2);
-   Vector2 v2(sf::Vector2f(2,7));
-
-    cout << (v1+v2).print() << endl;
-    cout << (v1-v2).print() << endl;
-    cout << (v1*v2).print() << endl;
-    cout << (v1/v2).print() << endl;
-
-    Entity* entity1 = new Entity(0,0);
-    entity1->createComponent<Physics>(entity1);
-    Physics* physComp = entity1->getComponent<Physics>();
-    physComp->velocity = Vector2(1,1);
-    physComp->update(1);
-    cout << entity1->position.print() << endl;
-    physComp->update(1);
-    cout << entity1->position.print() << endl;
-    physComp->update(.5);
-    cout << entity1->position.print() << endl;
 
     getchar();
     return 0;
