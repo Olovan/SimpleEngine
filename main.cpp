fc1ec18e47983e85c3e80791ec79442dbedcb79a
Olovan <micahsmith.92@gmail.com>
2015-12-27 21:42:33 -0500

    Added some test code to make sure the Game object is working properly

--- main.cpp
+++ main.cpp
@@ -23,7 +23,22 @@ public:
 int main()
 {
    //TEST CODE
+   Game* game = new Game();
+   Entity* currentEntity = game->createEntity(Vector2(0,0));
+   currentEntity->createComponent<Physics>(currentEntity);
+   currentEntity->createComponent<Renderer>(currentEntity, Vector2(1,1));
+   currentEntity = game->createEntity(Vector2(0,0));
+   currentEntity->createComponent<Physics>(currentEntity);
+   currentEntity->createComponent<Renderer>(currentEntity, Vector2(1,1));
+   currentEntity = game->createEntity(Vector2(0,0));
+   currentEntity->createComponent<Physics>(currentEntity);
+   currentEntity->createComponent<Renderer>(currentEntity, Vector2(1,1));
+   currentEntity = game->createEntity(Vector2(0,0));
+   currentEntity->createComponent<Physics>(currentEntity);
+   currentEntity->createComponent<Renderer>(currentEntity, Vector2(1,1));
 
+    cout << "Number of Renderers: " << game->renderers.size() << endl;
+    cout << "Number of Entities: " << game->entities.size() << endl;
     getchar();
     return 0;
 }
