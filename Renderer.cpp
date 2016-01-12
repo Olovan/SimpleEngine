aa8a68099909c059f403aba48f72a7e0f539de38
Micah Smith <micahsmith.92@gmail.com>
2016-01-11 23:31:29 -0500

    Set renderer origin to center of object and set Renderer to render the angle of the object as well as the position

--- Renderer.cpp
+++ Renderer.cpp
@@ -8,6 +8,7 @@ Renderer::Renderer(Entity* iEntity, Vector2 iDimensions) : Component(iEntity)
     dimensions = iDimensions;
     entity = iEntity;
     shape.setSize(dimensions.sfml());
+    shape.setOrigin(iDimensions.x / 2, iDimensions.y / 2);
 }
 
 Renderer::Renderer(Entity *iEntity, Vector2 iDimensions, sf::Color iColor ) : Component(iEntity)
@@ -16,6 +17,7 @@ Renderer::Renderer(Entity *iEntity, Vector2 iDimensions, sf::Color iColor ) : Co
     entity = iEntity;
     shape.setFillColor(iColor);
     shape.setSize(dimensions.sfml());
+    shape.setOrigin(iDimensions.x / 2, iDimensions.y / 2);
 }
 
 Renderer::Renderer(Entity *entity, Vector2 iDimensions, std::string path) : Component(entity)
@@ -23,6 +25,7 @@ Renderer::Renderer(Entity *entity, Vector2 iDimensions, std::string path) : Comp
     dimensions = iDimensions;
     texture = entity->game->textureManager.getTexture(path);
     shape.setSize(dimensions.sfml());
+    shape.setOrigin(iDimensions.x / 2, iDimensions.y / 2);
     if(texture)
         shape.setTexture(texture,false);
     else
@@ -43,6 +46,7 @@ void Renderer::start() //insert self into Game renderers set
 void Renderer::update(float deltaTime)
 {
     shape.setPosition(entity->position.x, entity->position.y);
+    shape.setRotation(entity->angle);
 }
 
 std::shared_ptr<Component> Renderer::clone()
