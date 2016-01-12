a2cb188e4fb7be49dcbb42f3f04eddb7e132e2ac
Micah Smith <micahsmith.92@gmail.com>
2016-01-11 23:33:07 -0500

    Set up basic physics Demo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <Box2D/Box2D.h>
 #include <SFML/Graphics.hpp>
 #include "Vector2.h"
 #include "Game.h"
@@ -8,6 +9,7 @@
             #include "Physics.h"
             #include "Renderer.h"
             #include "CustomClasses.h"
+            #include "BoxCollider.h"
 
 #include <time.h>
 
@@ -25,47 +27,37 @@ int main()
     srand(time(NULL));
 
     Entity* currentEntity;
-    currentEntity = game->createEntity(Vector2(645,478));
-    currentEntity->createComponent<Physics>(currentEntity, Vector2(80,80));
+    currentEntity = game->createEntity(Vector2(200,400));
+//    currentEntity->createComponent<Physics>(currentEntity, Vector2(80,80));
     currentEntity->createComponent<Renderer>(currentEntity, Vector2(50,50), sf::Color::Red);
-    currentEntity->createComponent<BouncingSquare>(currentEntity);
+//    currentEntity->createComponent<BouncingSquare>(currentEntity);
+    currentEntity->createComponent<BoxCollider>(currentEntity, Vector2(50,50), b2_dynamicBody);
 
-    currentEntity = game->createEntity(Vector2(200,178));
-    currentEntity->createComponent<Physics>(currentEntity, Vector2(140,280));
-    currentEntity->createComponent<Renderer>(currentEntity, Vector2(200,50), sf::Color::Yellow);
-    currentEntity->createComponent<BouncingSquare>(currentEntity);
+    currentEntity = game->createEntity(Vector2(460,390));
+    currentEntity->createComponent<Renderer>(currentEntity, Vector2(50,50), sf::Color::Blue);
+    currentEntity->createComponent<BoxCollider>(currentEntity, Vector2(50,50), b2_dynamicBody);
 
-    currentEntity = game->createEntity(Vector2(40,678));
-    currentEntity->createComponent<Renderer>(currentEntity, Vector2(5,5), sf::Color::Cyan);
-    currentEntity->createComponent<BouncingSquare>(currentEntity);
-    currentEntity->createComponent<Physics>(currentEntity, Vector2(400,600));
+    currentEntity = game->createEntity(Vector2(220,340));
+    currentEntity->createComponent<Renderer>(currentEntity, Vector2(50,50), sf::Color::Green);
+    currentEntity->createComponent<BoxCollider>(currentEntity, Vector2(50,50), b2_dynamicBody);
 
-    currentEntity = game->createEntity(Vector2(40,678));
-    currentEntity->createComponent<Renderer>(currentEntity, Vector2(5,5), sf::Color::Cyan);
-    currentEntity->createComponent<BouncingSquare>(currentEntity);
-    currentEntity->createComponent<Physics>(currentEntity, Vector2(400,600));
 
-    currentEntity = game->createEntity(Vector2(40,678));
-    currentEntity->createComponent<Renderer>(currentEntity, Vector2(100,100), "./pictures/1.png");
-    currentEntity->createComponent<BouncingSquare>(currentEntity);
-    currentEntity->createComponent<Physics>(currentEntity, Vector2(400,600));
+    currentEntity = game->createEntity(Vector2(260,180));
+    currentEntity->createComponent<Renderer>(currentEntity, Vector2(50,50), sf::Color::Yellow);
+    currentEntity->createComponent<BoxCollider>(currentEntity, Vector2(50,50), b2_dynamicBody);
 
-    currentEntity = game->createEntity(Vector2(40,678));
-    currentEntity->createComponent<Renderer>(currentEntity, Vector2(10,10), "./pictures/1.png");
-    currentEntity->createComponent<BouncingSquare>(currentEntity);
-    currentEntity->createComponent<Physics>(currentEntity, Vector2(200,600));
+    currentEntity = game->createEntity(Vector2(110,240));
+    currentEntity->createComponent<Renderer>(currentEntity, Vector2(50,50), sf::Color::Blue);
+    currentEntity->createComponent<BoxCollider>(currentEntity, Vector2(50,50), b2_dynamicBody);
 
-    currentEntity = game->createEntity(Vector2(40,678));
-    currentEntity->createComponent<Renderer>(currentEntity, Vector2(100,100), "./Pictures/1.png");
-    currentEntity->createComponent<BouncingSquare>(currentEntity);
-    currentEntity->createComponent<Physics>(currentEntity, Vector2(400,100));
 
-    currentEntity = game->createEntity(Vector2(40,678));
-    currentEntity->createComponent<Renderer>(currentEntity, Vector2(100,100), "./pictuRes/1.png");
-    currentEntity->createComponent<BouncingSquare>(currentEntity);
-    currentEntity->createComponent<Physics>(currentEntity, Vector2(100,300));
+    currentEntity = game->createEntity(Vector2(0,700));
+    currentEntity->createComponent<Renderer>(currentEntity, Vector2(500,50), sf::Color::Yellow);
+    currentEntity->createComponent<BoxCollider>(currentEntity,Vector2(500,50), b2_staticBody);
 
-    cout << game->textureManager.textures.size() << " Texture detected" << endl;
+
+
+    cout << game->textureManager.textures.size() << " Textures detected" << endl;
 
 //    Entity* currentEntity2 = game->createEntity(Vector2(400,400));
 //    currentEntity2->createComponent<Duplicator>(currentEntity2, currentEntity);
